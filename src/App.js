028aa40f9375c9748b4957dd8726a18edfd70d46
{eunnnnnnn} <pje9978@gmail.com>
2023-10-17 11:41:00 +0900

    버튼 누르면 리스트 생성 todo list

--- src/App.js
+++ src/App.js
@@ -5,26 +5,36 @@ import Button from "./Button"
 import { useState ,useEffect, useRef} from "react";
 
 function App() {
-  const [toDo, setTodo]  = useState("");
-  const [toDos, setTodos]  = useState([]);
+  const [toDo, setTodo] = useState("");
+  const [toDos, setToDos] = useState([]);
   const onChange = (event) => setTodo(event.target.value);
+  // console.log(toDo)
   const onSubmit = (event) => {
     event.preventDefault();
-    if(toDo === "") {
+    // console.log(toDo)
+    if(toDo === ""){
       return;
     }
-
-    setTodos((currentArray) => [toDo, ...currentArray]);
+    
+    setToDos((currentArray) => [toDo, ...currentArray]);
     setTodo("");
   }
-  console.log(toDos)
-  // console.log(toDo)
+ 
   return (
     <>
-      <form  onSubmit={onSubmit}>
-        <input value={toDo}  onChange={onChange} type="text" placeholder='write your to do..' />
+      <h1>My to do list {toDos.length}</h1>
+      <form action="" onSubmit={onSubmit}>
+        <input
+        onChange={onChange}
+        value={toDo}
+        type="text"
+        placeholder="Write your to do..."/>
         <button>Add to do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item,index) => (<li key={index}>{item}</li>))}
+      </ul>
     </>
   );
 }
