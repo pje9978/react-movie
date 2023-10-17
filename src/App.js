b7dac3ae9f4eb0f45c31d187080a74645713a4cb
{eunnnnnnn} <pje9978@gmail.com>
2023-10-17 12:06:50 +0900

    로딩이 되면 api 데이터 불러오기 useeffect

--- src/App.js
+++ src/App.js
@@ -5,36 +5,25 @@ import Button from "./Button"
 import { useState ,useEffect, useRef} from "react";
 
 function App() {
-  const [toDo, setTodo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setTodo(event.target.value);
-  // console.log(toDo)
-  const onSubmit = (event) => {
-    event.preventDefault();
-    // console.log(toDo)
-    if(toDo === ""){
-      return;
-    }
-    
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setTodo("");
-  }
- 
+   const [loading, setLoading] = useState(true);
+   const [coin, setCoins] = useState([])
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response)  => response.json())
+      .then((json) => {
+        setCoins(json); 
+        setLoading(false);
+      });
+  },[])
   return (
     <>
-      <h1>My to do list {toDos.length}</h1>
-      <form action="" onSubmit={onSubmit}>
-        <input
-        onChange={onChange}
-        value={toDo}
-        type="text"
-        placeholder="Write your to do..."/>
-        <button>Add to do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item,index) => (<li key={index}>{item}</li>))}
-      </ul>
+      <h1>The coins {loading ? "" : `(${coin.length})`}</h1>
+      {loading ? <strong>Loading</strong> : 
+        <ul>
+            {coin.map((coin)=> <li>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD</li>)}
+        </ul>
+      }
+
     </>
   );
 }
