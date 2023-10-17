1ee3d09b6423bbebc9a76dca2c91d25020733111
{eunnnnnnn} <pje9978@gmail.com>
2023-10-17 12:37:06 +0900

    영화  데이터 api로 가져오기 useEffect

--- src/App.js
+++ src/App.js
@@ -5,25 +5,41 @@ import Button from "./Button"
 import { useState ,useEffect, useRef} from "react";
 
 function App() {
-   const [loading, setLoading] = useState(true);
-   const [coin, setCoins] = useState([])
-  useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response)  => response.json())
-      .then((json) => {
-        setCoins(json); 
-        setLoading(false);
-      });
-  },[])
-  return (
-    <>
-      <h1>The coins {loading ? "" : `(${coin.length})`}</h1>
-      {loading ? <strong>Loading</strong> : 
-        <ul>
-            {coin.map((coin)=> <li>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD</li>)}
-        </ul>
-      }
+  const [loading,setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const json = await (await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+    )).json();
 
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
+  useEffect(()=>{
+    getMovies()
+  },[])
+  console.log(movies)
+  return (
+    
+    <>
+      {loading? (
+        <h1>Loaing</h1>
+      ) : ( 
+        <div> 
+          {movies.map(movie => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image}></img>
+              <h2 className='text-2xl'>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul className='bg-yellow-200'>
+                {movie.genres.map(g => <li>{g}</li>)}
+                
+              </ul>
+            </div>
+           
+          ))}
+        </div>
+      )}
     </>
   );
 }
