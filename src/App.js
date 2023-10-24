03e845d1f4f721e3626782395f18d98c81b6fbe1
{eunnnnnnn} <pje9978@gmail.com>
2023-10-24 13:46:39 +0900

    id 라우터 연결

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
-
+import Movie from "./components/Movie";
 function App() {
   return (
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Detail" element={<Detail />} />
+        <Route path="/Detail/:id" element={<Detail />} />
+       
       
       </Routes>
 
