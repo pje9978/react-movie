e074728e40ee3b93f6dcb8053a9d2f258c3a776b
{eunnnnnnn} <pje9978@gmail.com>
2023-10-26 11:44:46 +0900

    basename 삭제

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
-import Movie from "./components/Movie";
+
 function App() {
   return (
 
       <Routes>
-        <Route path={process.env.PUBLIC_URL + "/"}  element={<Home />} />
-        <Route path={process.env.PUBLIC_URL + "/movie/:id"}  element={<Detail />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<Detail />} />
 
       
       </Routes>
